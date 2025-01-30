98e954a4e893bc7da089f17d11564302cbbeb6b0
Emmanuel Sánchez <emmsanchezaranda@hotmail.com>
2025-01-29 22:02:07 -0600

    Styling chat

--- src/app/components/Chat/Chat.tsx
+++ src/app/components/Chat/Chat.tsx
@@ -1,7 +1,8 @@
 import { useStore } from "@/store/store";
-import { ChatForm } from "./ChatForm";
 import { AnimatePresence } from "motion/react";
 import { motion } from "motion/react";
+import { HeroChatForm } from "./HeroChatForm";
+import { DynamicChatForm } from "./DynamicChatForm";
 
 export default function Chat() {
   const messages = useStore((state) => state.messages);
@@ -20,27 +21,42 @@ export default function Chat() {
               type: "tween",
               ease: "backInOut",
             }}
-            className="flex flex-col gap-4 justify-center items-center w-[840px] px-4"
+            className="flex flex-col gap-4 justify-center items-center w-full px-4"
           >
+            <div className="absolute -z-20 h-full w-[1000px] bg-white">
+              <div className="absolute h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1.5px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]"></div>
+            </div>
+
             <h2 className="text-4xl text-center font-bold">
-              Your{" "}
-              <span className="bg-gradient-to-br from-[#3aaaf6] to-[#0470c5] text-transparent bg-clip-text">
-                AI-Powered
-              </span>{" "}
-              Crypto Companion
+              Your AI-Powered Crypto Companion
             </h2>
             <p className="max-w-sm text-base text-center text-zinc-600">
               Automated, user-friendly chat for token swaps and crypto insights.
             </p>
 
-            <ChatForm />
+            <HeroChatForm />
           </motion.div>
         ) : (
-          <ul>
-            {messages.map((message, index) => {
-              return <li key={index}>{message.content}</li>;
-            })}
-          </ul>
+          <div className="w-full flex justify-center px-4">
+            <ul className="flex flex-col w-[1024px] h-dvh gap-2 pb-16 bg-cyan-300 overflow-y-auto">
+              {messages.map((message, index) => {
+                return (
+                  <li
+                    key={index}
+                    className={`flex px-4 py-2 bg-white text-black border-solid border-[1px] border-zinc-300 rounded-xl shadow-md ${
+                      message.role === "user" ? "self-end" : "self-start"
+                    }`}
+                  >
+                    {message.content}
+                  </li>
+                );
+              })}
+            </ul>
+
+            <div className="absolute bottom-0 pb-2">
+              <DynamicChatForm />
+            </div>
+          </div>
         )}
       </AnimatePresence>
     </>
