f6b2debe1fdb16c2bbc179d4b715aeb3270c38d5
Emmanuel Sánchez <emmsanchezaranda@hotmail.com>
2025-02-20 01:26:47 -0600

    Starting DB

---
+++ src/app/controllers/chatService.ts
@@ -0,0 +1,3 @@
+export async function saveChat(req, res) {}
+
+export async function fetchChats(req, res) {}
---
+++ src/app/model/postgresql/chatModel.ts
@@ -0,0 +1,9 @@
+export async function getChatsByUser(userId: string) {
+  try {
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+export async function createChat() {}
