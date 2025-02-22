5fff82d3add8089c4dbe6a58db9348c36041af86
Emmanuel Sánchez <emmsanchezaranda@hotmail.com>
2025-02-22 01:16:38 -0600

    Model, controller and route for login on DB

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,22 @@
+import { logInUser } from "@/app/controllers/chatController";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const { userId, username } = await req.json();
+    if (!userId || !username) {
+      return NextResponse.json({
+        success: false,
+        message: "Id or username is empty",
+      });
+    }
+
+    const user = await logInUser(userId, username);
+    return NextResponse.json({ succes: true, user }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, message: error },
+      { status: 500 }
+    );
+  }
+}
--- src/app/model/postgresql/chatModel.ts
+++ src/app/model/postgresql/chatModel.ts
@@ -1,9 +1,47 @@
-export async function getChatsByUser(userId: string) {
+import pg from "pg";
+
+const { Pool } = pg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_PUBLIC_URL,
+});
+
+export async function findOrCreateUser(userId: string, username: string) {
   try {
+    const existingUser = await pool.query(
+      "SELECT * FROM users WHERE privy_user_id = $1",
+      [userId]
+    );
+
+    // If the user exists just return
+    if (existingUser.rowCount) {
+      console.log("user exists");
+
+      return existingUser.rows[0];
+    }
+
+    // If not exists, save it
+    const newUser = await pool.query(
+      "INSERT INTO users (privy_user_id, username) VALUES ($1, $2) RETURNING *",
+      [userId, username]
+    );
+
+    console.log("new user");
+    return newUser.rows[0];
   } catch (error) {
     console.error(error);
     return [];
   }
 }
 
-export async function createChat() {}
+// export async function getChatsByUser(userId: string) {
+//   try {
+//   } catch (error) {
+//     console.error(error);
+//     return [];
+//   }
+// }
+
+// export async function createChat() {}
+
+// export async function addNewUser() {}
