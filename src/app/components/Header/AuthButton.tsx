6b6050e63371a0b0c76cb313c662e9c423d0a44a
Emmanuel Sánchez <emmsanchezaranda@hotmail.com>
2025-03-11 00:56:59 -0600

    Saving user info

--- src/app/components/Header/AuthButton.tsx
+++ src/app/components/Header/AuthButton.tsx
@@ -1,15 +1,18 @@
 "use client";
 
+import { useUserStore } from "@/store/store";
 import { usePrivy } from "@privy-io/react-auth";
 import { LogIn, LogOutIcon } from "lucide-react";
 import React, { useEffect } from "react";
 
 export default function AuthButton() {
+  const setUserInfo = useUserStore((state) => state.setUserInfo);
+  const userInfo = useUserStore((state) => state.userInfo);
   const { login, logout, authenticated, user } = usePrivy();
 
   useEffect(() => {
     if (authenticated && user) {
-      const login = async () => {
+      const loginInDB = async () => {
         try {
           const data = await fetch("/api/auth/login", {
             method: "POST",
@@ -21,14 +24,19 @@ export default function AuthButton() {
               username: user.email?.address || user.wallet?.address,
             }),
           });
-          console.log(data);
+          const jsonData = await data.json();
+
+          if (jsonData.succes) {
+            const { id, username } = jsonData.user;
+            setUserInfo(id, username);
+          }
         } catch (error) {
           console.error(error);
           throw new Error("Error getting user data");
         }
       };
 
-      login();
+      loginInDB();
     }
   }, [authenticated]);
 
@@ -38,9 +46,12 @@ export default function AuthButton() {
         <>
           <button
             onClick={logout}
-            className="flex size-7 justify-center items-center"
+            className="flex h-7 justify-center items-center gap-2 px-4 dark:hover:bg-zinc-600 border border-zinc-600 rounded-full"
           >
-            <LogOutIcon className="size-4 dark:invert" />
+            <span className="text-sm text-zinc-200">
+              {userInfo.username.slice(0, 5)}
+            </span>
+            <LogOutIcon className="size-[14px] dark:invert" />
           </button>
         </>
       ) : (
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,17 +1,48 @@
 import { create } from "zustand";
-import { Message } from "@/types/types";
+// import { Message } from "@/types/types";
 
-interface ChatState {
-  prompt: string;
-  setPrompt: (newPrompt: string) => void;
-  messages: Message[];
-  addMessage: (newMessage: Message) => void;
+// interface ChatState {
+//   prompt: string;
+//   setPrompt: (newPrompt: string) => void;
+//   messages: Message[];
+//   addMessage: (newMessage: Message) => void;
+// }
+
+// export const useStore = create<ChatState>((set) => ({
+//   prompt: "",
+//   setPrompt: (newPrompt: string) => set({ prompt: newPrompt }),
+//   messages: [],
+//   addMessage: (newMessage: Message) =>
+//     set((state) => ({ messages: [...state.messages, newMessage] })),
+// }));
+
+interface UserState {
+  userInfo: {
+    id: string;
+    username: string;
+  };
+  setUserInfo: (id: string, username: string) => void;
+  clearUser: () => void;
 }
 
-export const useStore = create<ChatState>((set) => ({
-  prompt: "",
-  setPrompt: (newPrompt: string) => set({ prompt: newPrompt }),
-  messages: [],
-  addMessage: (newMessage: Message) =>
-    set((state) => ({ messages: [...state.messages, newMessage] })),
+export const useUserStore = create<UserState>((set) => ({
+  userInfo: {
+    id: "",
+    username: "",
+  },
+  setUserInfo: (id: string, username: string) =>
+    set({ userInfo: { id, username } }),
+  clearUser: () => set({ userInfo: { id: "", username: "" } }),
+}));
+
+interface ChatState {
+  chatId: string | null;
+  setChatId: (id: string) => void;
+  clearChatId: () => void;
+}
+
+export const useChatState = create<ChatState>((set) => ({
+  chatId: null,
+  setChatId: (id: string) => set({ chatId: id }),
+  clearChatId: () => set({ chatId: null }),
 }));
